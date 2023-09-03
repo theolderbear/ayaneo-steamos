cc1bbe5f65889a7dddb4266c24e5a16f9bf6e63f
Paolo Luciani <beartheolder@gmail.com>
2023-09-03 16:51:05 +0200

    rclone

---
+++ rclone/run_rclone_restore.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+source $HOME/emudeck/settings.sh
+PIDFILE="$toolsPath/rclone/rclone.pid"
+
+function finish {
+  echo "Script terminating. Exit code $?"
+}
+trap finish EXIT
+
+if [ -z "$savesPath" ] || [ -z "$rclone_provider" ]; then
+    echo "You need to setup your cloudprovider first."
+    exit
+fi
+
+if [ -f "$PIDFILE" ]; then
+  PID=$(cat "$PIDFILE")
+  ps -p "$PID" > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > "$PIDFILE"
+    if [ $? -ne 0 ]; then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > "$PIDFILE"
+  if [ $? -ne 0 ]; then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+"$toolsPath/rclone/rclone" copy -L "$rclone_provider":Emudeck/saves "$savesPath" -P > "$toolsPath/rclone/rclone_job.log"
+
